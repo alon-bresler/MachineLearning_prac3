ce7a4993291a705261f1aead98ebd9bac1d60a37
Alon Bresler <abresler22@gmail.com>
2015-08-26 16:44:31 +0200

    Q1 working - with all weights initially set to zero, it is done it one itereation.

--- main.cpp
+++ main.cpp
@@ -3,53 +3,46 @@
 
 using namespace std;
 
-//PERCEPTRON RULE//
-//weight(new) = weight(old) + n(t)x
-//linear activation function
-float perceptronLearningRule(float w, float n, int t, int x)
-{
-    return w + (n*t*x);
-}
+//VARIABLES//
+int trainingData[4][5] = {{1, -1, 1, -1, 1},{1, 1, 1, 1, 1},{1, 1, 1, -1, -1},{1, -1, -1, 1, -1}};
+int perceptrons[4] = {0, 0, 0, 0};
+float threshold = 0.0f;
+float learningRate = 0.1f;
+//random initial weights for inputs//
+//float weight[4] = {rand()/float(RAND_MAX), rand()/float(RAND_MAX), rand()/float(RAND_MAX), rand()/float(RAND_MAX)};
+float weight[4] = {0.0f, 0.0f, 0.0f, 0.0f};
 
-int linearPerceptron(float w_i, float weight[], int x[])
+int linearPerceptron(int x[])
 {
-    if (w_i + weight[0]*x[0] + weight[1]*x[1] + weight[2]*x[2] + weight[3]*x[3] > 0)
+    if ((weight[0]*x[0]) + (weight[1]*x[1]) + (weight[2]*x[2]) + (weight[3]*x[3]) >  threshold)
     {
         return 1;
     }
     else
     {
-        return 0;
+        return -1;
     }
 }
 
 int main()
 {
-    int trainingData[4][5] = {{1, -1, 1, -1, 1},{1, 1, 1, 1, 1},{1, 1, 1, -1, -1},{1, -1, -1, 1, -1}};
-    int perceptrons[4];
-
-    float threshold = -0.5f;
-    float learningRate = 0.1f;
-
-    //random initial weights for inputs//
-    float weight[4] = {rand()/float(RAND_MAX), rand()/float(RAND_MAX), rand()/float(RAND_MAX), rand()/float(RAND_MAX)};
-
     int error = 4;
+    int iterations = 0;
 
-    while (error =! 0)
+    while (error != 0)
     {
-        int error = 4;
+        error = 4;
 
-        //adjust weights//
         for (int k = 0; k < 4; k++)
         {
+            //adjust weights//
             for (int i = 0; i < 4; i++)
             {
-                weight[i] = perceptronLearningRule(weight[i], learningRate, trainingData[i][4], trainingData[k][i]);
+                weight[i] += learningRate * (trainingData[k][4] - perceptrons[k]) * trainingData[k][i];
             }
 
             //get linear Perception//
-            perceptrons[k] = linearPerceptron(threshold, weight, trainingData[k]);
+            perceptrons[k] = linearPerceptron(trainingData[k]);
 
         }
 
@@ -60,15 +53,20 @@ int main()
                 error--;
         }
 
+        //increment iterations//
+        iterations++;
+
         cout << endl;
-        cout << "weight 1: " << weight[0] << endl;
-        cout << "weight 2: " << weight[1] << endl;
-        cout << "weight 3: " << weight[2] << endl;
-        cout << "weight 4: " << weight[3] << endl;
+        cout << "p 1: " << perceptrons[0] << endl;
+        cout << "p 2: " << perceptrons[1] << endl;
+        cout << "p 3: " << perceptrons[2] << endl;
+        cout << "p 4: " << perceptrons[3] << endl;
+        cout << "error: " << error << endl;
         cout << endl;
 
      }
 
+    cout << "\nIterations:" << iterations << "\n" << endl;
     cout << "Done" << endl;
 
     return 0;
